fca049c69d420994febec8d871c0f28cb734472e
VeronicaFontana <fontana.veronica98@gmail.com>
2023-11-29 15:00:52 +0100

    First commit [ADD] Routes, migration, seeder

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 50; $i++){
+            $new_project = new Project();
+            $new_project->name = $faker->sentence();
+            $new_project->slug = Project::generateSlug($new_project->name);
+            $new_project->description = $faker->paragraph();
+            $new_project->creation_date = $faker->date();
+
+            $new_project->save();
+        }
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.admin")
+
+@section("content")
+
+    <h1>Home Admin</h1>
+
+@endsection
